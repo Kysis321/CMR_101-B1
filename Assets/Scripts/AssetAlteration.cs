e1da9fff197599cf072b33a9e50418ddb69e77a6
Kyle <1021455@student.sae.edu.au>
2020-10-26 13:39:47 +1100

    Full commitment of B1 Files with the correct Github setup now!

---
+++ Assets/Scripts/AssetAlteration.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class AssetAlteration : MonoBehaviour
+{
+    public float amountToRotate; //amount of degrees to rotate on interaction
+
+    public TextMesh HUD;
+    public float widthToExpand = 0.5f;
+    public float heightToExpand = 0.5f;
+    public float depthToExpand = 0.5f;
+    public bool ableToBeDestroyed;
+
+    Renderer r;
+    Renderer rr;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+        rr = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+
+
+
+    /// <summary>
+    /// size alterations
+    /// </summary>
+    public void MakeBig()
+    {
+        // Transforms the object to expand in all three dimensions on activation
+        transform.localScale = new Vector3(transform.localScale.x + widthToExpand, transform.localScale.y + heightToExpand, transform.localScale.z + depthToExpand);
+
+    }
+
+    public void MakeStairBig()
+    {
+        // Transforms the object to expand in all three dimensions on activation
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + heightToExpand, transform.localScale.z);
+
+    }
+
+    public void MakeStairSmall()
+    {
+        // Transforms the object to expand in all three dimensions on activation
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - 0.3f, transform.localScale.z);
+
+    }
+    public void MakeSmall()
+    {
+
+        transform.localScale = new Vector3(transform.localScale.x - 0.3f, transform.localScale.y - 0.3f, transform.localScale.z - 0.3f);
+
+    }
+
+    /// <summary>
+    /// Rotation Alterations
+    /// </summary>
+    /// 
+    public void RotateRight()
+    {
+        transform.Rotate(0.0f, -amountToRotate, 0.0f, Space.Self);
+        HUD.text = transform.rotation.ToString();
+
+    }
+
+    public void RotateLeft()
+    {
+        transform.Rotate(0.0f, amountToRotate, 0.0f, Space.Self);
+        HUD.text = transform.rotation.ToString();
+
+    }
+
+    ///<summary>
+    ///This will move the object into a direction
+    /// </summary>
+    public void MoveLeft()
+    {
+        transform.Translate(UnityEngine.Vector3.left);
+        HUD.text = transform.position.ToString();
+
+    }
+
+    public void MoveRight()
+    {
+        transform.Translate(UnityEngine.Vector3.right);
+        HUD.text = transform.position.ToString();
+    }
+
+
+    public void changeYellow()
+    {
+        r.material.color = Color.yellow;
+    }
+
+    public void returnColour()
+    {
+       
+    }
+
+    // Next function acts to desstroy the door if a player doesnt walk up to it
+
+    void DestroyDoor()
+    {
+        if(ableToBeDestroyed == true)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+
+
+
+
+
+}
+    
---
+++ Assets/Scripts/RaycastShoot.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShoot : MonoBehaviour
+{
+    public float fireRate = .25f; // Sets firetime of weapon
+    public float weaponRange = 50f; //range of raycast max
+    private Camera headCam;
+
+    public Transform gunEnd;
+
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.7f);
+    private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire; //after firing - how long to the next shot
+
+
+
+
+
+
+    void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetButtonDown("Trigger") && Time.time > nextFire)
+        {
+
+            nextFire = Time.time + fireRate;
+
+            StartCoroutine(ShotEffect());
+
+            Vector3 rayOrigin = headCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+
+            laserLine.SetPosition(0, gunEnd.position);
+            
+            if (Physics.Raycast(rayOrigin,headCam.transform.forward,out hit, weaponRange))
+            {
+
+                laserLine.SetPosition(1, hit.point);
+
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (headCam.transform.forward * weaponRange));
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    private IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+            
+
+
+    }
+
+
+
+}
+
+
+
